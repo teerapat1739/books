5990b0cf33d309aa1178e941a0909c9ab4ddfbdb
Teerapat Thong-O <teerapat@gopomelo.com>
2021-08-08 15:30:46 +0700

    format date format respose function

--- src/lib/format/response/user.js
+++ src/lib/format/response/user.js
@@ -6,7 +6,14 @@ function format_response(data) {
   data.forEach(({ book_id }) => {
     books.push(book_id);
   });
-  return { ...data[0], books };
+  return {
+    ...data[0],
+    date_of_birth: DateTime.local().toFormat(
+      "dd/MM/yyyy",
+      data[0].date_of_birth
+    ),
+    books,
+  };
 }
 
 module.exports = {
